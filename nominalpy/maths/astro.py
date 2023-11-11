116f7efcb41e848c2c989d313572a0a1aac26d6b
brentonwhf <brenno90@live.com.au>
2023-11-11 12:30:34 +1100

    adding a classical_to_vector_elements that takes input as radians

--- nominalpy/maths/astro.py
+++ nominalpy/maths/astro.py
@@ -14,6 +14,7 @@ from . import constants
 from . import value
 from .. import NominalException
 
+
 def get_planet_mu (planet: str) -> float:
     '''
     Returns the planet's gravitational mu parameter, equal to GM of the
@@ -30,29 +31,49 @@ def get_planet_mu (planet: str) -> float:
     else:
         raise NominalException("No planet or body named '%s' exists." % planet)
 
-def classical_to_vector_elements (semi_major_axis: float, eccentricity: float = 0.0,
+
+def classical_to_vector_elements_rad(semi_major_axis: float, eccentricity: float = 0.0,
     inclination: float = 0.0, right_ascension: float = 0.0, argument_of_periapsis: float = 0.0,
     true_anomaly: float = 0.0, planet: str = "earth") -> tuple:
-    '''
-    Calculates the state vector (both a position and a velocity) in the
-    units of meters and meters per second respectively, from a classical
-    Keplerian element set. The Keplerian elements require the semi-major
-    axis (SMA) to be in kilometers, the eccentricity to be between 0 and 1
-    and the other four elements to be in degrees. The orbiting body will
-    define the planet being orbited. The output of this function will
-    return two JSON formatted strings for the Vector3 values, in the form of:
-        (position, velocity).
-    '''
+    """
+    Converts classical Keplerian elements to state vector (position and velocity).
+
+    This function calculates the state vector from a set of classical
+    Keplerian elements. The semi-major axis should be in kilometers, eccentricity
+    should be between 0 and 1, and the angular elements (inclination, right ascension,
+    argument of periapsis, and true anomaly) should be in radians. The function
+    returns the position and velocity in meters and meters per second, respectively,
+    formatted as JSON strings.
+
+    :param semi_major_axis: The semi-major axis in kilometers.
+    :type semi_major_axis: float
+    :param eccentricity: The orbital eccentricity (default 0.0).
+    :type eccentricity: float
+    :param inclination: The inclination in radians (default 0.0).
+    :type inclination: float
+    :param right_ascension: The right ascension in radians (default 0.0).
+    :type right_ascension: float
+    :param argument_of_periapsis: The argument of periapsis in radians (default 0.0).
+    :type argument_of_periapsis: float
+    :param true_anomaly: The true anomaly in radians (default 0.0).
+    :type true_anomaly: float
+    :param planet: The name of the planet being orbited (default "earth").
+    :type planet: str
+    :return: A tuple containing position and velocity as JSON-formatted strings.
+    :rtype: Tuple[str, str]
+
+    .. note:: The function assumes that the orbiting body is Earth unless otherwise specified.
+    """
     # Define a small numerical value
     EPS: float = 1e-12
 
     # Store local values from the input in the correct form
     a: float = semi_major_axis * 1000.0
     e: float = eccentricity
-    i: float = inclination * constants.D2R
-    an: float = right_ascension * constants.D2R
-    ap: float = argument_of_periapsis * constants.D2R
-    f: float = true_anomaly * constants.D2R
+    i: float = inclination
+    an: float = right_ascension
+    ap: float = argument_of_periapsis
+    f: float = true_anomaly
     mu: float = get_planet_mu(planet)
 
     # Keep track of the new variables for the vector
@@ -97,9 +118,9 @@ def classical_to_vector_elements (semi_major_axis: float, eccentricity: float = 
         else:
             p = 2 * a * (1 - e * e) / (1 + e * math.cos(0))  # parabolic 
 
-        r = p / (1 + e * math.cos(f));      # orbit radius
-        theta = ap + f;                     # true latitude angle 
-        h = math.sqrt(mu * p);              # orbit ang. momentum mag.
+        r = p / (1 + e * math.cos(f))      # orbit radius
+        theta = ap + f                     # true latitude angle
+        h = math.sqrt(mu * p)              # orbit ang. momentum mag.
 
         rVec[0] = r * (math.cos(an) * math.cos(theta) - math.sin(an) * math.sin(theta) * math.cos(i))
         rVec[1] = r * (math.sin(an) * math.cos(theta) + math.cos(an) * math.sin(theta) * math.cos(i))
@@ -118,4 +139,47 @@ def classical_to_vector_elements (semi_major_axis: float, eccentricity: float = 
     # Return the new elements
     position: str = value.vector3(rVec[0], rVec[1], rVec[2])
     velocity: str = value.vector3(vVec[0], vVec[1], vVec[2])
-    return (position, velocity)
+    return position, velocity
+
+
+def classical_to_vector_elements(semi_major_axis: float, eccentricity: float = 0.0,
+    inclination: float = 0.0, right_ascension: float = 0.0, argument_of_periapsis: float = 0.0,
+    true_anomaly: float = 0.0, planet: str = "earth") -> tuple:
+    """
+    Converts classical Keplerian elements to state vector (position and velocity).
+
+    This function calculates the state vector from a set of classical
+    Keplerian elements. The semi-major axis should be in kilometers, eccentricity
+    should be between 0 and 1, and the angular elements (inclination, right ascension,
+    argument of periapsis, and true anomaly) should be in degrees. The function
+    returns the position and velocity in meters and meters per second, respectively,
+    formatted as JSON strings.
+
+    :param semi_major_axis: The semi-major axis in kilometers.
+    :type semi_major_axis: float
+    :param eccentricity: The orbital eccentricity (default 0.0).
+    :type eccentricity: float
+    :param inclination: The inclination in degrees (default 0.0).
+    :type inclination: float
+    :param right_ascension: The right ascension in degrees (default 0.0).
+    :type right_ascension: float
+    :param argument_of_periapsis: The argument of periapsis in degrees (default 0.0).
+    :type argument_of_periapsis: float
+    :param true_anomaly: The true anomaly in degrees (default 0.0).
+    :type true_anomaly: float
+    :param planet: The name of the planet being orbited (default "earth").
+    :type planet: str
+    :return: A tuple containing position and velocity as JSON-formatted strings.
+    :rtype: Tuple[str, str]
+
+    .. note:: The function assumes that the orbiting body is Earth unless otherwise specified.
+    """
+    classical_to_vector_elements_rad(
+        semi_major_axis=semi_major_axis,
+        eccentricity=eccentricity,
+        inclination=inclination * constants.D2R,
+        argument_of_periapsis=argument_of_periapsis * constants.D2R,
+        right_ascension=right_ascension * constants.D2R,
+        true_anomaly=true_anomaly * constants.D2R,
+        planet=planet
+    )
