fcb9e15059ae6070a418ab84d80a413c30bd9733
brentonwhf <brenno90@live.com.au>
2023-11-16 12:29:46 +1100

    adding shortcuts to the Object and Simulation classes

--- nominalpy/__init__.py
+++ nominalpy/__init__.py
@@ -5,3 +5,5 @@ from .objects.message import Message
 from .printer import log, error, warning, success, set_verbosity, LOG_VERBOSITY, SUCCESS_VERBOSITY, WARNING_VERBOSITY, ERROR_VERBOSITY
 from .objects.simulation import Simulation
 from .types import *
+from .objects.object import Object
+from .objects.simulation import Simulation
