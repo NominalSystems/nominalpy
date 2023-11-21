f93e69c4b1e248274c7ba2d7b2140cbab28f6cc3
Brenton Smith <brenton.smith@unsw.edu.au>
2023-11-21 16:19:45 +1100

    fixing geodetic function names, sanitising numpy float arrays, get param

--- src/nominalpy/maths/astro.py
+++ src/nominalpy/maths/astro.py
@@ -1,5 +1,5 @@
 #                     [ NOMINAL SYSTEMS ]
-# This code is developed by Nominal Systems to aid with communication 
+# This code is developed by Nominal Systems to aid with communication
 # to the public API. All code is under the the license provided along
 # with the 'nominalpy' module. Copyright Nominal Systems, 2023.
 
@@ -16,7 +16,28 @@ from . import utils
 from ..utils import NominalException
 
 
-def get_planet_mu (planet: str) -> float:
+def get_planet_property(planet: str, property: str) -> float:
+    """
+    Returns the planet's property. If the planet does not exist, then an exception will be thrown.
+
+    :param planet: The name of the planet to fetch the property from
+    :type planet: str
+    :param property: The name of the property to fetch
+    :type property: str
+    :return: The value of the property
+    :rtype: float
+    """
+    # Check if the value is valid
+    param = f"{planet.upper()}_{property.upper()}"
+    if hasattr(constants, param):
+        return float(getattr(constants, param))
+
+    # Throw an error if this occurs
+    else:
+        raise NominalException(f"No planet: {planet} with property {property}.")
+
+
+def get_planet_mu(planet: str) -> float:
     '''
     Returns the planet's gravitational mu parameter, equal to GM of the
     planet's body in units of [m^3/s^2]. If the planet does not exist,
@@ -28,15 +49,8 @@ def get_planet_mu (planet: str) -> float:
     :returns:       The gravitational MU parameter
     :rtype:         float
     '''
-    
-    # Check if the value is valid
-    param = "%s_MU" % planet.upper()
-    if hasattr(constants, param):
-        return float(getattr(constants, param))
-    
-    # Throw an error if this occurs
-    else:
-        raise NominalException("No planet or body named '%s' exists." % planet)
+    return get_planet_property(planet, "MU")
+
 
 def t_perifocal_to_vector_elements (
     right_ascension: float = 0,
@@ -44,15 +58,15 @@ def t_perifocal_to_vector_elements (
     inclination: float = 0
 )-> np.ndarray:
     '''
-    Creates the transformation matrix to convert perifocal coordinates into 
+    Creates the transformation matrix to convert perifocal coordinates into
     Planet-Centered Inertial (PCI) coordinates.
 
-    This function computes the transformation matrix using the right ascension 
-    of the ascending node, the argument of periapsis, and the orbital inclination. 
-    These parameters are expected to be in radians. The transformation matrix 
-    is computed based on classical orbital elements, and it's used in orbital 
-    mechanics to convert coordinates from the perifocal (or orbital plane) 
-    coordinate system, where the x-axis points towards the periapsis, to the 
+    This function computes the transformation matrix using the right ascension
+    of the ascending node, the argument of periapsis, and the orbital inclination.
+    These parameters are expected to be in radians. The transformation matrix
+    is computed based on classical orbital elements, and it's used in orbital
+    mechanics to convert coordinates from the perifocal (or orbital plane)
+    coordinate system, where the x-axis points towards the periapsis, to the
     PCI coordinate system.
 
     :param right_ascension:         The right ascension of the ascending node in radians
@@ -79,6 +93,7 @@ def t_perifocal_to_vector_elements (
     ], dtype=np.float64)
     return TIP
 
+
 def perifocal_to_vector_elements(
     r_bp_p: np.ndarray,
     v_bp_p: np.ndarray,
@@ -87,14 +102,14 @@ def perifocal_to_vector_elements(
     argument_of_periapsis: float = 0,
 ) -> tuple:
     '''
-    Converts position and velocity vectors from perifocal to Planet-Centered 
+    Converts position and velocity vectors from perifocal to Planet-Centered
     Inertial (PCI) coordinates.
 
-    Given position and velocity vectors in the perifocal coordinate system (PQW), 
-    along with orbital elements such as right ascension of the ascending node, 
-    inclination, and argument of perigee, this function transforms these vectors 
-    into the ECI coordinate system. The transformation is performed using a matrix 
-    obtained from the `t_perifocal_to_vector` function, which constructs the 
+    Given position and velocity vectors in the perifocal coordinate system (PQW),
+    along with orbital elements such as right ascension of the ascending node,
+    inclination, and argument of perigee, this function transforms these vectors
+    into the ECI coordinate system. The transformation is performed using a matrix
+    obtained from the `t_perifocal_to_vector` function, which constructs the
     transformation matrix based on the provided orbital elements.
 
     :param r_bp_p:                  Position vector in perifocal coordinates.
@@ -111,7 +126,10 @@ def perifocal_to_vector_elements(
     :returns:                       Tuple of transformed position and velocity vectors in ECI coordinates.
     :rtype:                         (numpy.ndarray, numpy.ndarray)
     '''
-
+    # ensure that the input arrays are of type float
+    r_bp_p = r_bp_p.astype(np.float64)
+    v_bp_p = v_bp_p.astype(np.float64)
+    # calculate the transformation matrix
     TIP = t_perifocal_to_vector_elements(
         right_ascension=right_ascension,
         argument_of_periapsis=argument_of_periapsis,
@@ -121,6 +139,7 @@ def perifocal_to_vector_elements(
     r_bn_n = TIP @ r_bp_p
     v_bn_n = TIP @ v_bp_p
     return r_bn_n, v_bn_n
+
 
 def semi_latus_rectum_to_vector_elements(
     semi_latus_rectum: float,
@@ -132,15 +151,15 @@ def semi_latus_rectum_to_vector_elements(
     planet: str = "earth"
 ) -> tuple:
     '''
-    Transforms Keplerian orbital elements into position and velocity in Planet-Centered 
-    Inertial (PCI) coordinates. by taking semi-latus rectum as direct input, this 
+    Transforms Keplerian orbital elements into position and velocity in Planet-Centered
+    Inertial (PCI) coordinates. by taking semi-latus rectum as direct input, this
     function is valid for all orbits including parabolic and hyperbolic.
 
-    Given the Keplerian orbital elements, this function calculates the orbital state 
-    (position and velocity) in the PCI coordinate system. This transformation is essential 
-    in astrodynamics for orbit propagation and analysis. The function first computes the 
-    gravitational parameter of the planet, then calculates the semi-latus rectum, and 
-    finally computes the position and velocity in perifocal coordinates. These are then 
+    Given the Keplerian orbital elements, this function calculates the orbital state
+    (position and velocity) in the PCI coordinate system. This transformation is essential
+    in astrodynamics for orbit propagation and analysis. The function first computes the
+    gravitational parameter of the planet, then calculates the semi-latus rectum, and
+    finally computes the position and velocity in perifocal coordinates. These are then
     transformed into PCI coordinates.
 
     :param semi_latus_rectum:       Semi-latus rectum of the orbit in meters
@@ -171,13 +190,13 @@ def semi_latus_rectum_to_vector_elements(
         semi_latus_rectum * np.cos(true_anomaly) / denom,
         semi_latus_rectum * np.sin(true_anomaly) / denom,
         0
-    ])
+    ], dtype=np.float64)
     rat = np.sqrt(mu / semi_latus_rectum)
     v_bp_p = np.array([
         -rat * np.sin(true_anomaly),
         rat * (eccentricity + np.cos(true_anomaly)),
         0
-    ])
+    ], dtype=np.float64)
     # calculate the pci position and velocity
     return perifocal_to_vector_elements(
         r_bp_p=r_bp_p,
@@ -187,6 +206,7 @@ def semi_latus_rectum_to_vector_elements(
         inclination=inclination
     )
 
+
 def classical_to_vector_elements(
     semi_major_axis: float,
     eccentricity: float = 0.0,
@@ -195,15 +215,15 @@ def classical_to_vector_elements(
     argument_of_periapsis: float = 0.0,
     true_anomaly: float = 0.0,
     planet: str = "earth"
-) -> tuple:
+) -> np.ndarray:
     '''
-    Transforms Keplerian orbital elements into position and velocity in Planet-Centered 
+    Transforms Keplerian orbital elements into position and velocity in Planet-Centered
     Inertial (PCI) coordinates.
 
-    Given the Keplerian orbital elements, this function calculates the orbital state 
+    Given the Keplerian orbital elements, this function calculates the orbital state
     (position and velocity) in the PCI coordinate system. This transformation is
-    essential in astrodynamics for orbit propagation and analysis. The function first 
-    computes the gravitational parameter of the planet, then calculates the semi-latus 
+    essential in astrodynamics for orbit propagation and analysis. The function first
+    computes the gravitational parameter of the planet, then calculates the semi-latus
     rectum, and finally computes the position and velocity in perifocal coordinates.
     These are then transformed into PCI coordinates.
 
@@ -222,8 +242,8 @@ def classical_to_vector_elements(
     :param planet:                  Name of the central body being orbited (default "earth")
     :type planet:                   str
 
-    :returns:                       A tuple containing position and velocity vectors in PCI coordinates
-    :rtype:                         tuple
+    :returns:
+    :rtype:
     '''
 
     # calculate the semi-latus rectum
@@ -236,7 +256,7 @@ def classical_to_vector_elements(
         p = semi_major_axis * (1 - eccentricity * eccentricity)
     else:
         raise ValueError("The input eccentricity is invalid")
-    v = semi_latus_rectum_to_vector_elements(
+    return semi_latus_rectum_to_vector_elements(
         semi_latus_rectum=p,
         eccentricity=eccentricity,
         inclination=inclination,
@@ -245,7 +265,6 @@ def classical_to_vector_elements(
         true_anomaly=true_anomaly,
         planet=planet,
     )
-    return (value.vector3(v[0][0], v[0][1], v[0][2]), value.vector3(v[1][0], v[1][1], v[1][2]))
 
 
 def classical_to_vector_elements_deg(
@@ -258,14 +277,14 @@ def classical_to_vector_elements_deg(
     planet: str = "earth"
 ) -> tuple:
     '''
-    Transforms Keplerian orbital elements into position and velocity in Planet-Centered 
+    Transforms Keplerian orbital elements into position and velocity in Planet-Centered
     Inertial (PCI) coordinates.
 
-    Given the Keplerian orbital elements, this function calculates the orbital state 
-    (position and velocity) in the PCI coordinate system. This transformation is 
-    essential in astrodynamics for orbit propagation and analysis. The function first 
-    computes the gravitational parameter of the planet, then calculates the semi-latus 
-    rectum, and finally computes the position and velocity in perifocal coordinates. 
+    Given the Keplerian orbital elements, this function calculates the orbital state
+    (position and velocity) in the PCI coordinate system. This transformation is
+    essential in astrodynamics for orbit propagation and analysis. The function first
+    computes the gravitational parameter of the planet, then calculates the semi-latus
+    rectum, and finally computes the position and velocity in perifocal coordinates.
     These are then transformed into PCI coordinates.
 
     :param semi_major_axis:         Semi-major axis of the orbit in meters
@@ -304,14 +323,14 @@ def vector_to_classical_elements(
     planet: str = "earth"
 ) -> tuple:
     '''
-    Convert state vectors to classical orbital elements. This function 
-    calculates the classical orbital elements from the given position 
-    and velocity vectors. It supports different planetary bodies by 
-    adjusting the gravitational parameter. The function calculates 
-    various orbital elements including angular momentum, node vector, 
-    eccentricity vector, semi-major axis, inclination, RAAN, argument 
-    of periapsis, and true anomaly. It handles different types of orbits: 
-    circular equatorial, equatorial elliptical, circular inclined, and 
+    Convert state vectors to classical orbital elements. This function
+    calculates the classical orbital elements from the given position
+    and velocity vectors. It supports different planetary bodies by
+    adjusting the gravitational parameter. The function calculates
+    various orbital elements including angular momentum, node vector,
+    eccentricity vector, semi-major axis, inclination, RAAN, argument
+    of periapsis, and true anomaly. It handles different types of orbits:
+    circular equatorial, equatorial elliptical, circular inclined, and
     classical elliptical inclined.
 
     :param r_bn_n:              The position vector in a Cartesian coordinate system in metres.
@@ -326,6 +345,9 @@ def vector_to_classical_elements(
 
     :raises ArithmeticError:    If the orbit is parabolic, which is not supported by this function.
     '''
+    # ensure that the input arrays are of type float
+    r_bn_n = r_bn_n.astype(np.float64)
+    v_bn_n = v_bn_n.astype(np.float64)
 
     # find the gravitational parameter for the planet
     mu: float = get_planet_mu(planet)
@@ -398,23 +420,28 @@ def vector_to_classical_elements(
         )
 
 
-def ecef_to_lla(position: np.ndarray) -> np.ndarray:
+def pcpf_to_geodetic_lla(position: np.ndarray, planet="Earth") -> np.ndarray:
     """
-    Converts the Earth-Centred, Earth-Fixed parameters to Latitude, Longitude, and Altitude using NumPy.
+    Converts the Planet-Centred, Planet-Fixed parameters to Latitude, Longitude, and Altitude using NumPy.
 
     :param position: The current ECPF position as a NumPy array or list (X, Y, Z)
     :type position: np.ndarray
+    :param planet: The planet to use for the conversion
+    :type planet: str
+
     :return: Tuple containing Latitude [rad], Longitude [rad], and Altitude [m]
     :rtype: np.ndarray
     """
+    # ensure that the input array is of type float
+    position = position.astype(np.float64)
     # handle the edge case where the position is zero
     if np.linalg.norm(position) == 0:
         return np.array([0, 0, 0], dtype=np.float64)
     # make sure that the position is of type float
     position = position.astype(dtype=np.float64)
     # WGS Parameters
-    a: float = constants.EARTH_REQ  # Equatorial radius of Earth
-    f: float = 1.0 / 298.257  # Flattening factor of Earth
+    a: float = get_planet_property(planet=planet, property="REQ")  # Equatorial radius of the planet
+    f: float = get_planet_property(planet=planet, property="FLATTENING")  # Flattening factor of the planet
     e: float = np.sqrt(2 * f - f ** 2)
 
     longitude: float = np.arctan2(position[1], position[0])
@@ -446,36 +473,43 @@ def ecef_to_lla(position: np.ndarray) -> np.ndarray:
     return np.array([latitude, longitude, altitude], dtype=np.float64)
 
 
-def ecef_to_lla_deg(position: np.ndarray) -> np.ndarray:
+def pcpf_to_geodetic_lla_deg(position: np.ndarray, planet="Earth") -> np.ndarray:
     """
-    Converts the Earth-Centred, Earth-Fixed parameters to Latitude, Longitude, and Altitude using NumPy.
+    Converts the Planet-Centred, Planet-Fixed parameters to Latitude, Longitude, and Altitude using NumPy.
 
     :param position: The current ECPF position as a NumPy array or list (X, Y, Z)
     :type position: np.ndarray
+    :param planet: The planet to use for the conversion
+    :type planet: str
+
     :return: Tuple containing Latitude [deg], Longitude [deg], and Altitude [m]
     :rtype: np.ndarray
     """
-    lla = ecef_to_lla(position)
+    lla = pcpf_to_geodetic_lla(position, planet=planet)
     return np.array([np.degrees(lla[0]), np.degrees(lla[1]), lla[2]], dtype=np.float64)
 
 
-def lla_to_ecef(lla: np.ndarray) -> np.ndarray:
+def geodetic_lla_to_pcpf(lla: np.ndarray, planet="Earth") -> np.ndarray:
     """
-    Converts from Latitude/Longitude/Altitude (LLA) coordinates to Earth-Centered,
+    Converts from Latitude/Longitude/Altitude (LLA) coordinates to Planet-Centered,
     Planet-Fixed (PCPF) coordinates given a planet radius.
 
     :param lla: A tuple or list containing Latitude (radians), Longitude (radians),
                         and Altitude (meters) coordinates.
+    :type lla: np.ndarray
+    :param planet: The planet to use for the conversion
+    :type planet: str
     :return: A tuple representing the final position in the PCPF frame in meters.
     """
-
+    # ensure that the input array is of type float
+    lla = lla.astype(np.float64)
     # Convert latitude and longitude from degrees to radians
     lat: float = lla[0]  # Latitude in radians
     lon: float = lla[1]  # Longitude in radians
     alt: float = lla[2]  # Altitude in meters
-    f: float = 1 / 298.257  # Flattening factor
+    planet_radius: float = get_planet_property(planet=planet, property="REQ")
+    f: float = get_planet_property(planet=planet, property="FLATTENING")  # Flattening factor
     e_sq: float = f * (2 - f)  # Square of eccentricity
-    planet_radius: float = constants.EARTH_REQ
 
     # Calculate the radius of curvature in the prime vertical
     N: float = planet_radius / np.sqrt(1 - e_sq * np.sin(lat) ** 2)
@@ -488,20 +522,24 @@ def lla_to_ecef(lla: np.ndarray) -> np.ndarray:
     return np.array([X, Y, Z], dtype=np.float64)
 
 
-def lla_to_ecef_deg(lla: np.ndarray) -> np.ndarray:
+def geodetic_lla_to_pcpf_deg(lla: np.ndarray, planet="Earth") -> np.ndarray:
     """
-    Converts from Latitude/Longitude/Altitude (LLA) coordinates to Earth-Centered,
+    Converts from Latitude/Longitude/Altitude (LLA) coordinates to Planet-Centered,
     Planet-Fixed (PCPF) coordinates given a planet radius.
 
     :param lla: A tuple or list containing Latitude (degrees), Longitude (degrees),
                         and Altitude (meters) coordinates.
+    :type lla: np.ndarray
+    :param planet: The planet to use for the conversion
+    :type planet: str
     :return: A tuple representing the final position in the PCPF frame in meters.
+    :rtype: np.ndarray
     """
 
     # Convert latitude and longitude from degrees to radians
     lat: float = np.radians(lla[0])  # Latitude in radians
     lon: float = np.radians(lla[1])  # Longitude in radians
-    return lla_to_ecef(np.array([lat, lon, lla[2]]))
+    return geodetic_lla_to_pcpf(np.array([lat, lon, lla[2]], dtype=np.float64), planet=planet)
 
 
 def calculate_orbital_velocity(r_bn_n_mag: float, sma: float, gm: float = constants.EARTH_MU) -> float:
