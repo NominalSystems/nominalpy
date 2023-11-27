c41343f28d3590776eb3db1cfdc8df4d4d9bdfb3
Harrison Verrios <harrison.v1@live.com>
2023-11-27 13:50:38 +1100

    Finalized the python library for the v0.7.0 release.

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+#                     [ NOMINAL SYSTEMS ]
+# This code is developed by Nominal Systems to aid with communication 
+# to the public API. All code is under the the license provided along
+# with the 'nominalpy' module. Copyright Nominal Systems, 2023.
+
 from setuptools import setup, find_packages
 
 # Setup the project
@@ -10,6 +15,9 @@ setup(
     author='Nominal Systems',
     author_email='support@nominalsys.com',
     description='Python Interface to the Nominal API for simulations',
-    long_description="Python Interface to the Nominal API for simulations",
+    long_description="This is the Python interface library for the Nominal API. \
+        It enables accessing the REST API simulation functions in an easy format.\
+        Examples of how to construct Nominal simulations are provided on the\
+        Nominal Systems documentation found at https://www.docs.nominalsys.com.",
     url='https://api.nominalsys.com',
 )
