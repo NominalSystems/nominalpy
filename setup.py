67a40295ec8225e25501ca52000670e81b3cc96c
Harrison Verrios <harrison.v1@live.com>
2025-02-02 13:27:33 +1100

    The setup was missing the 'request' library, which is still being used.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #                     [ NOMINAL SYSTEMS ]
-# This code is developed by Nominal Systems to aid with communication 
+# This code is developed by Nominal Systems to aid with communication
 # to the public API. All code is under the the license provided along
 # with the 'nominalpy' module. Copyright Nominal Systems, 2024.
 
@@ -7,19 +7,29 @@ from setuptools import setup, find_packages
 
 # Setup the project
 setup(
-    name='nominalpy',
-    version='1.1.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    install_requires=["aiohttp", "urllib3", "paho-mqtt", "numpy", "pandas", "matplotlib", "setuptools", "pytest-asyncio"],
-    author='Nominal Systems',
-    author_email='support@nominalsys.com',
-    description='Python Interface to the Nominal API for simulations',
+    name="nominalpy",
+    version="1.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "aiohttp",
+        "urllib3",
+        "paho-mqtt",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "setuptools",
+        "pytest-asyncio",
+        "requests",
+    ],
+    author="Nominal Systems",
+    author_email="support@nominalsys.com",
+    description="Python Interface to the Nominal API for simulations",
     long_description="This is the Python interface library for the Nominal API. \
         The Nominal API allows access to spacecraft and space-domain simulation functions \
         and the framework for simulating high-fidelity satellites. \
         It enables accessing the REST API simulation functions in an easy format.\
         Examples of how to construct Nominal simulations are provided on the\
         Nominal Systems documentation found at https://docs.nominalsys.com.",
-    url='https://github.com/NominalSystems/nominalpy',
+    url="https://github.com/NominalSystems/nominalpy",
 )
